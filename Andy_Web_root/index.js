ec86f65e79090d12415fbf92b666d9945fb34a57
Andy Qin <56485866+AndyQYH@users.noreply.github.com>
2021-11-03 16:59:33 -0700

    Add files via upload
    1st Commit

---
+++ Andy_Web_root/index.js
@@ -0,0 +1,22 @@
+//My very first JS code! WOOHOO
+
+//vARIABLES
+let Name = 'Andy';
+let age = 19;
+
+//objects
+let person = {
+    name: 'Andy',
+    age:19
+}
+
+//arrays
+let Colors = ['red','green','blue'];
+
+//functions
+function greet(NAME){
+    console.log('Hello '+ NAME);
+}
+
+greet(Name);
+
